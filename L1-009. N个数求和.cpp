a3a737bbc61288d6a94a00d8f42fc4075d9485a0
QiuZhiQing <55350792+7845932@users.noreply.github.com>
2020-11-17 09:44:33 +0800

    Update L1-009. N个数求和.cpp

--- L1-009. N个数求和.cpp
+++ L1-009. N个数求和.cpp
@@ -1,35 +1,38 @@
-#include <cstdio>
-#include <cstdlib>
+#include<cstdio>
+#include<cstdlib>
 using namespace std;
-long long gcd(long long a, long long b) {return b == 0 ? a : gcd(b, a % b);}
-int main() {
-    long long n, a, b, suma = 0, sumb = 1, gcdvalue;
-    scanf("%lld", &n);
-    for(int i = 0; i < n; i++) {
-        scanf("%lld/%lld", &a, &b);
-        gcdvalue = (suma == 0 || sumb == 0) ? 1 : gcd(abs(suma), abs(sumb));
-        sumb = sumb / gcdvalue;
-        suma = suma / gcdvalue;
-        gcdvalue = (a == 0 || b == 0) ? 1 : gcd(abs(a), abs(b));
-        a = a / gcdvalue;
-        b = b / gcdvalue;
-        suma = a * sumb + suma * b;
-        sumb = b * sumb;
-    }
-    long long integer = suma / sumb;
-    suma = suma - (sumb * integer);
-    gcdvalue = (suma == 0 || sumb == 0) ? 1 : gcd(abs(suma), abs(sumb));
-    suma = suma / gcdvalue;
-    sumb = sumb / gcdvalue;
-    if(integer != 0) {
-        printf("%lld", integer);
-        if(suma != 0)
-            printf(" ");
-    }
-    if(suma != 0) {
-        printf("%lld/%lld", suma, sumb);
-    }
-    if(integer == 0 && suma == 0)
-        printf("0");
-    return 0;
+long long gcd(long long a,long long b){return b==0?a:gcd(b,a%b);}
+int main(){
+	long long  n,suma=0,sumb=1,a,b,gcdvalue;
+	scanf("%lld", &n);
+	for(int i=0;i<n;i++){
+		scanf("%lld/%lld",&a,&b);
+		
+		gcdvalue=(suma==0||sumb==0)?1:gcd(abs(suma),abs(sumb));
+		suma=suma/gcdvalue;
+		sumb=sumb/gcdvalue;
+		gcdvalue=(a==0||b==0)?1:gcd(abs(a),abs(b));
+		a=a/gcdvalue;
+		b=b/gcdvalue;
+		suma+=suma*b+sumb*a;
+		sumb+=sumb*b;
+	}
+	long long integer=suma/sumb;
+	suma=suma-integer*sumb;
+	gcdvalue=(suma==0||sumb==0)?1:gcd(abs(suma),abs(sumb));
+		suma=suma/gcdvalue;
+		sumb=sumb/gcdvalue;
+	if(integer!=0){
+		printf("%lld",integer);
+		
+		if(suma!=0){
+			printf(" ");
+		}
+	}
+	if(suma!=0){
+		printf("%lld/%lld",suma,sumb);
+	}
+	if(integer==0&&suma==0)
+	printf("0");
+	
 }
